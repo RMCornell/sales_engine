6eab2ad05b5d6aa1c44475c25e76db2e0d3663a8
Rick Bacci <rickbacci@gmail.com>
2015-04-27 22:17:05 -0600

    create csv parser class

--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -1,5 +1,15 @@
+
 class CsvParser
 
+  def self.parse(file)
+    if File.exist?(file)
+      CSV.open(file, headers: true, header_converters: :symbol)
+    else
+      raise IOError, "#{file} not found!"
+    end
+  end
 end
+
+
 
 
--- test/csv_parser_test.rb
+++ test/csv_parser_test.rb
@@ -2,9 +2,18 @@ require_relative 'test_helper'
 
 class ParserTest < Minitest::Test
 
-  def test_parser
-    assert CsvParser.new
+  def setup
+    @file = '../data/customers.csv'
   end
 
+  def test_parser_can_load_a_file
+    assert CsvParser.parse(@file)
+  end
 
+  def test_parser_raises_error_if_file_is_not_found
+    #todo how to make this work?
+    skip
+    CsvParser.parse('./my/file')
+    assert_raises IOError, './file/not/found not found!'
+  end
 end
