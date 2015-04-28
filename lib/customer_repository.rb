ce42237c257b215e098f6220c3db2cdd2c3b3bab
kurohyou108 <robert.cornell78@gmail.com>
2015-04-28 15:52:09 -0600

    Repo and Class Frameworks Outlined

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,14 +1,15 @@
  class CustomerRepository
-   attr_accessor :customer_data, :parent, :customers,
+   attr_reader :customer_data, :parent, :customers, :engine
 
   def initialize(customer_data, parent)
     @parent = parent
     @customer_data   = customer_data
     @customers = load_customers(customer_data)
+    @engine = engine
   end
 #
   def load_customers(customer_data)
-    @customer_data.map do |row|
+    customer_data.map do |row|
       Customer.new(row, self)
     end
   end
@@ -18,11 +19,11 @@
   end
 #
   def find_invoices_by_customer_id(id)
-    @engine.find_invoices_by_customer_id(@id)
+    engine.find_invoices_by_customer_id(id)
   end
 #
   def find_customer_by_id(id)
-    @customers[id]
+    customers[id]
   end
 #
 end
