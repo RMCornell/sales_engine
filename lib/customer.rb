43ede4a818904c6044db36a7059d3957971d9f1f
Rick Bacci <rickbacci@gmail.com>
2015-04-28 21:04:26 -0600

    add rest of customer attributes

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,17 +1,18 @@
 class Customer
 
-  attr_accessor :repository, :id, :first_name
+  attr_reader :repository, :id, :first_name, :last_name,
+              :created_at, :modified_at
 
   def initialize(customer, repository)
-    @id         = customer[:id].to_i
-    @first_name = customer[:first_name]
-    @repository = repository
-
+     @repository = repository
+     @id         = customer[:id].to_i
+     @first_name = customer[:first_name]
+     @last_name  = customer[:last_name]
+     @created_at = customer[:created_at]
+    @modified_at = customer[:modified_at]
   end
 
-
-
   def invoices
-    repository.find_invoices_by_customer_id(@id)
+    repository.find_invoices_by_customer_id(id)
   end
 end
