95b37e298b5d3e4ceee773eee837545761af5599
Rick Bacci <rickbacci@gmail.com>
2015-05-01 07:16:59 -0600

    initial commit

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -14,17 +14,27 @@ class Merchant
     @updated_at = merchant[:updated_at]
   end
 
+
   def inspect
     "#<#{self.class}: id:#{@id.inspect} name: #{@name.inspect}  created_at: #{@created_at.inspect} updated_at: #{@updated_at.inspect}>"
   end
 
+
+  ### merchant(id) --> invoices(merchant_id)
+  # merchant#invoices
+
   def items
-# todo items returns a collection of Item instances associated with that merchant for the products they sell
+    
   end
+
+
+  ### merchant(id) --> items(merchant_id)
+  # merchant#items
 
   def invoices
 # todo invoices returns a collection of Invoice instances associated with that merchant from their known orders
   end
+
 end
 
 
