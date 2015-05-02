48119783a039282f7505d73ae3cb406a62f2de38
kurohyou108 <robert.cornell78@gmail.com>
2015-05-02 13:45:44 -0600

    Item Repository Tests (WIP)

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -29,27 +29,38 @@ class ItemRepository
     items.detect { |item| id == item.id }
   end
 
+#Find_by Methods
   def find_by_name(name)
     items.detect {|item| name == item.name}
   end
 
   def find_by_description(description)
-    
+    items.detect{|item| description == item.description}
   end
 
   def find_by_unit_price(unit_price)
-
+    items.detect{|item| unit_price == item.unit_price}
   end
 
   def find_by_merchant_id(id)
     items.select { |item| item.merchant_id == id }
   end
 
-  def find_by_created_at(time)
-
+  def find_by_created_at(created_at)
+    items.detect {|item| created_at == item.created_at}
   end
 
-  def find_by_updated_at(time)
+  def find_by_updated_at(updated_at)
+    items.detect {|item| updated_at == item.created_at}
+  end
 
+#Find_all_by methods
+  def find_all_by_id(id)
+    items.select{|item| id == item.id}
+  end
+
+  def find_all_by_name(name)
+    items.select{|item| name == item.name}
+    items.select{|item| name == item.name}
   end
 end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -32,13 +32,54 @@ class ItemRepositoryTest < Minitest::Test
     assert_equal 100, total_random_items
   end
 
+#Find_by Methods
   def test_item_repository_returns_item_by_id
     by_id = items.find_by_id(5)
     assert_equal 68723, by_id.unit_price
   end
 
   def test_item_repository_returns_item_by_name
-    skip
-    by_name = items.find_by_name()
+    by_name = items.find_by_name("Item Provident At")
+    assert_equal 6, by_name.id
+  end
+
+  def test_item_repositoyr_returns_item_by_description
+    by_description = items.find_by_description("Numquam officiis reprehenderit eum ratione neque tenetur. Officia aut repudiandae eum at ipsum doloribus. Iure minus itaque similique. Ratione dicta alias asperiores minima ducimus nesciunt at.")
+    assert_equal 6, by_description.id
+  end
+
+  def test_item_repository_returns_item_by_unit_price
+    by_unit_price = items.find_by_unit_price(15925)
+    assert_equal 6, by_unit_price.id
+  end
+
+  def test_item_repository_retuns_item_by_merchant_id
+    by_merchant_id = items.find_by_merchant_id(1)
+    assert_equal 15, by_merchant_id.count
+  end
+
+  def test_item_repository_returns_item_by_created_at
+    by_created_at = items.find_by_created_at("2012-03-27 14:53:59 UTC")
+    assert_equal 1, by_created_at.id
+  end
+
+  def test_item_repository_returns_items_by_updated_at
+    by_updated_at = items.find_by_updated_at("2012-03-27 14:53:59 UTC")
+    assert_equal 1, by_updated_at.id
+  end
+
+#Find_by_all Methods
+  def test_item_repository_returns_all_items_by_id
+    all_by_id = items.find_all_by_id(1)
+    assert_equal 1, all_by_id.count
+    assert all_by_id.first.is_a?(Item)
+  end
+
+  def test_item_repository_returns_all_items_by_name
+    all_by_name = items.find_all_by_name("Item Provident At")
+    assert_equal 1, all_by_name.count
   end
 end
+
+#assert all_by_invoice_id.first.is_a?(InvoiceItem)
+
