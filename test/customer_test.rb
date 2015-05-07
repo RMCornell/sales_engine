e8aeb68ceba09995cf70ab9d74238ff58f1e713f
Rick Bacci <rickbacci@gmail.com>
2015-05-07 03:26:17 -0600

    add customer tests

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -1,45 +1,35 @@
 require_relative 'test_helper'
 
 class CustomerTest < Minitest::Test
-  attr_reader :engine, :customers
+  attr_reader :engine, :customers, :customer
 
   def setup
     @engine = SalesEngine.new('./test/fixtures')
     @customers = engine.customer_repository
-  end
-
-  def test_customers_can_exist
-     assert customers
+    @customer = engine.customer_repository.find_by_id(2)
   end
 
   def test_customers_have_a_repository
     refute customers.any? { |customer| customer.repository.nil? }
   end
 
-  def test_customers_have_an_id
+  def test_customers_attributes_are_not_nil
     refute customers.any? { |customer| customer.id.nil? }
-  end
-
-  def test_customers_have_a_first_name
     refute customers.any? { |customer| customer.first_name.nil? }
-  end
-
-  def test_customers_have_a_last_name
     refute customers.any? { |customer| customer.last_name.nil? }
-  end
-
-  def test_customer_knows_when_it_was_created
     refute customers.any? { |customer| customer.created_at.nil? }
-  end
-
-  def test_customer_knows_when_it_was_last_modified
-    # todo need these probably set to 'created_at'
     refute customers.any? { |customer| customer.updated_at.nil? }
   end
 
+  def test_can_return_customer_invoices
+    assert_equal 1, customer.invoices.size
+  end
 
-  
-  def test_sustomer_finds_favorite_merchant
+  def test_customer_transactions
+    assert_equal 1, customer.transactions.size
+  end
 
+  def test_customer_finds_favorite_merchant
+    assert_equal "Shields, Hirthe and Smith", customer.favorite_merchant.name
   end
 end
